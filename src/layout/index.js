9374df473ceb420ee4e6af190f2d518a8d0f9c63
kai <b10223052@yuntech.org.tw>
2020-03-08 15:38:02 +0800

    modify css style and add background

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { HashRouter as Router, } from 'react-router-dom';
 import LayoutRouter from '../route';
-import './style.scss';
 
 function Layout() {
 	return (
 		<Router>
-			<div className="home-page__content">
-				<LayoutRouter/>
-			</div>
+			<LayoutRouter/>
 		</Router>
 	);
 }
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,9 +5,7 @@ class Login extends Component {
 	render() {
 		return (
 			<div className="login">
-				<div>
 					Hello World!!!
-				</div>
 			</div>
 		);
 	}
