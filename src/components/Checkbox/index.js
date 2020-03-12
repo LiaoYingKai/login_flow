99f49ab8ef7a3df82347a0d7a5e5e950cf5db5e3
kai <b10223052@yuntech.org.tw>
2020-03-12 10:11:09 +0800

    add checkbox component

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import './style.scss';
+
+const propTypes = {
+	onChange: PropTypes.func,
+	className: PropTypes.string,
+	value: PropTypes.bool,
+};
+
+const defaultProps = {
+	onChange: () => {},
+};
+
+function Checkbox({
+	value,
+	onChange,
+	className
+}) {
+	return (
+		<div className={cx("checkbox", className)}>
+			<input
+				type="checkbox"
+				id="checkbox"
+				onChange={onChange}
+				value={value}
+			/>
+			<label htmlFor="checkbox"></label>
+		</div>
+	);
+}
+
+Checkbox.propTypes = propTypes;
+Checkbox.defaultProps = defaultProps;
+
+export default Checkbox;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,16 @@
 import React, { Component, } from 'react';
 import Button from '../../components/Button';
 import Icon from '../../components/Icon';
+import Checkbox from '../../components/Checkbox';
 import './style.scss';
 
 class Login extends Component {
+	constructor() {
+		super();
+		this.state = {
+			value: false,
+		};
+	}
 	render() {
 		return (
 			<div className="login">
@@ -16,6 +23,10 @@ class Login extends Component {
 					<div>
 						<Button type={Button.TypeEnums.TEXT}>Forget Password?</Button>
 					</div>
+					<Checkbox
+						onChange={(event) => {this.setState({ value: event.target.value });}}
+						value={this.state.value}
+					/>
 				</div>
 				<div className="login__prompt">
 					Dont have a account?
