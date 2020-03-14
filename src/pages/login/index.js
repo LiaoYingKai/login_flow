7995d0716fd6650ce1660c894130d17b150f0173
kai <b10223052@yuntech.org.tw>
2020-03-14 17:13:57 +0800

    modify layout

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,43 +1,66 @@
-import React, { Component, } from 'react';
+import React, { useState, } from 'react';
 import Button from '../../components/Button';
 import Icon from '../../components/Icon';
 import Checkbox from '../../components/Checkbox';
+import Input from '../../components/Input';
 import './style.scss';
 
-class Login extends Component {
-	constructor() {
-		super();
-		this.state = {
-			value: false,
-		};
-	}
-	render() {
-		return (
-			<div className="login">
-				<div className="login__content">
-					<div className="login__logo">
-						<Icon/>
-					</div>
-					<div className="login__content-title">Sign In</div>
-					<Button type={Button.TypeEnums.BUTTON}>Login</Button>
-					<div className="login__bottom">
-						<div>
-							<Checkbox
-								onChange={(event) => {this.setState({ value: event.target.value });}}
-								value={this.state.value}
-							/>
-							Remember Me
-						</div>
-						<Button type={Button.TypeEnums.TEXT}>Forget Password?</Button>
-					</div>
+const {
+	TypeEnums,
+	SizeEnums,
+	ColorEnums,
+} = Icon;
+
+const { MEDIUM } = SizeEnums;
+const { GRAY } = ColorEnums;
+const {
+	SOLID_USER,
+	REGULAR_USER,
+	SOLID_LOCK,
+} = TypeEnums;
+
+function Login() {
+	const [ account, setAccount ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ isRemember, setIsRemember ] = useState(false);
+
+
+	return (
+		<div className="login">
+			<div className="login__content">
+				<div className="login__logo">
+					<Icon type={REGULAR_USER}/>
 				</div>
-				<div className="login__prompt">
-					Dont have a account?
-					<Button type={Button.TypeEnums.TEXT}>DEGISTER HERE</Button>
+				<div className="login__content-title">Sign In</div>
+				<Input
+					prefixItem={<Icon size={MEDIUM} type={SOLID_USER} color={GRAY} />}
+					value={account}
+					onChange={(event) => {setAccount(event.target.value);}}
+				/>
+				<Input
+					prefixItem={<Icon size={MEDIUM} type={SOLID_LOCK} color={GRAY} />}
+					type='password'
+					value={password}
+					onChange={(event) => {setPassword(event.target.value);}}
+				/>
+				<Button type={Button.TypeEnums.BUTTON}>Login</Button>
+				<div className="login__bottom">
+					<div>
+						<Checkbox
+							onChange={(event) => {setIsRemember({ value: event.target.value });}}
+							value={isRemember}
+						/>
+						Remember Me
+					</div>
+					<Button type={Button.TypeEnums.TEXT}>Forget Password?</Button>
 				</div>
 			</div>
-		);
-	}
+			<div className="login__prompt">
+				Dont have a account?
+				<Button type={Button.TypeEnums.TEXT}>DEGISTER HERE</Button>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
