f6cbcd405d67aae62b1c1a0774729d5e979182a7
kai <b10223052@yuntech.org.tw>
2020-03-20 14:37:02 +0800

    add message modal component

---
+++ src/components/MessageModal/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import Button from '../Button';
+import './style.scss';
+
+const propTypes = {
+	className: PropTypes.string,
+	isVisible: PropTypes.bool,
+	title: PropTypes.string,
+	message: PropTypes.string,
+	onClickOk: PropTypes.func,
+	okText: PropTypes.string,
+};
+
+const defaultProps = {
+	isVisible: false,
+	title: '通知',
+	okText: '確認',
+	onClickOk: () => {},
+};
+
+function MessageModal({
+	className,
+	isVisible,
+	title,
+	message,
+	onClickOk,
+	okText,
+}) {
+	return (
+		<React.Fragment>
+			<div className={cx({ "message-modal--mock": isVisible, })}/>
+			<div className={cx(
+				className,
+				"message-modal",
+				{ "message-modal--hidden": !isVisible, }
+			)}>
+				<div className="message-modal__title">
+					{title}
+				</div>
+				<div className="message-modal__message">
+					{message}
+				</div>
+				<div className="message-modal__footer">
+					<Button
+						onClick={onClickOk}
+					>
+						{okText}
+					</Button>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+}
+
+MessageModal.propTypes = propTypes;
+MessageModal.defaultProps = defaultProps;
+
+export default MessageModal;
